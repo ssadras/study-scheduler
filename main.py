0e7f5b01ba46bb76759e3f1b6a5e31896199d3cb
Sadra <sadra3tarehdan@gmail.com>
2024-01-17 12:28:19 -0500

    database class created. Functionality list todo list added.

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+import scheduler
+
+db = scheduler.Database("database.sqlite3")
+db.create_connection()
+
 # Jeremy part 😅
--- scheduler.py
+++ scheduler.py
@@ -1,1 +1,30 @@
+import sqlite3
+from sqlite3 import Error
+
+
 # Sadra part 😎
+
+class Database:
+    def __init__(self, path):
+        self.path = path
+        self.conn = None
+
+    def create_connection(self):
+        """ create a database connection to a SQLite database """
+
+        if self.conn is not None:
+            return self.conn
+
+        try:
+            self.conn = sqlite3.connect(self.path)
+            print("Connection to SQLite DB successful")
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+        return self.conn
+
+    def create_table(self, create_table_sql):
+        """ create a table from the create_table_sql statement """
+        # todo: create the table and database structure: (functionalities needed: plans, optional plans, due dates,
+        #  unexpected events, meetings, next event, show overall schedule
+        pass
