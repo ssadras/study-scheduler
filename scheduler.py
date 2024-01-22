6e5764b2dcfc7b5284c4cec000755be97099bdcf
Sadra <sadra3tarehdan@gmail.com>
2024-01-22 09:10:56 -0500

    functions added for working with the database but nothing has been tested yet.

--- scheduler.py
+++ scheduler.py
@@ -34,14 +34,75 @@ class Database:
 
         return self.conn
 
+    def insert(self, insert_sql):
+        """ insert a row into a table from the insert_sql statement """
+
+        try:
+            self.conn.execute(insert_sql)
+            print("Row inserted successfully")
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+        return self.conn
+
+    def insert_with_output_id(self, insert_sql):
+        """ insert a row into a table from the insert_sql statement and return the id of the inserted row """
+
+        cursor = None
+
+        try:
+            cursor = self.conn.execute(insert_sql)
+            print("Row inserted successfully")
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+        return cursor.lastrowid
+
+    def select(self, select_sql):
+        """ select rows from a table from the select_sql statement """
+
+        cursor = None
+
+        try:
+            cursor = self.conn.execute(select_sql)
+            print("Rows selected successfully")
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+        return cursor
+
+    def update(self, update_sql):
+        """ update rows from a table from the update_sql statement """
+
+        try:
+            self.conn.execute(update_sql)
+            print("Rows updated successfully")
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+        return self.conn
+
+    def delete(self, delete_sql):
+        """ delete rows from a table from the delete_sql statement """
+
+        try:
+            self.conn.execute(delete_sql)
+            print("Rows deleted successfully")
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+        return self.conn
+
 
 class Scheduler:
     def __init__(self, path):
         self.db = Database(path)
 
-    def create_table(self):
+    def create_tables(self):
+        """ create tables in the database """
+
         user_table_sql = """CREATE TABLE IF NOT EXISTS User (
-                                        UserID INT PRIMARY KEY,
+                                        UserID INT PRIMARY KEY AUTO_INCREMENT,
                                         Username VARCHAR(255) NOT NULL,
                                         Name VARCHAR(255),
                                         Email VARCHAR(255)
@@ -49,13 +110,13 @@ class Scheduler:
                                     """
 
         recurring_activity_table_sql = """CREATE TABLE IF NOT EXISTS RecurringActivity (
-                                        RecurringActivityID INT PRIMARY KEY,
+                                        RecurringActivityID INT PRIMARY KEY AUTO_INCREMENT,
                                         RecurrencePattern VARCHAR(50) NOT NULL,
                                         RecurrenceEndDate DATE
                                     );"""
 
         activities_table_sql = """CREATE TABLE IF NOT EXISTS Activity (
-                                        ActivityID INT PRIMARY KEY,
+                                        ActivityID INT PRIMARY KEY AUTO_INCREMENT,
                                         UserID INT,
                                         Title VARCHAR(255),
                                         Description TEXT,
@@ -78,3 +139,76 @@ class Scheduler:
             return False
 
         return True
+
+    def insert_user(self, user_id, username, name, email):
+        """ insert a user into the database, if the user already exists, return False """
+
+        select_sql = f"""SELECT * FROM User WHERE Username = {username};"""
+
+        cursor = self.db.select(select_sql)
+
+        if cursor is not None:
+            print("User already exists")
+            return False
+
+        insert_sql = f"""INSERT INTO User (Username, Name, Email)
+                        VALUES ('{username}', '{name}', '{email}');"""
+
+        try:
+            self.db.insert(insert_sql)
+        except Exception as e:
+            print(f"The error '{e}' occurred")
+            return False
+
+        return True
+
+    def get_user(self, username):
+        """ get a user from the database """
+
+        select_sql = f"""SELECT * FROM User WHERE Username = {username};"""
+
+        cursor = self.db.select(select_sql)
+
+        if cursor is None:
+            print("User does not exist")
+            return None
+
+        return cursor
+
+    def insert_activity(self, username, title, description, type, date, start_time, end_time, is_recurring,
+                        recurring_pattern, recurring_end_date):
+        """ insert an activity into the database """
+
+        recurring_activity_id = None
+
+        if is_recurring:
+            recurring_activity_id = self.insert_recurring_activity(recurring_pattern, recurring_end_date)
+
+        user_id = self.get_user(username)
+        if user_id is None:
+            print("User does not exist")
+            return False
+
+        user_id = user_id[0]
+
+        insert_sql = f"""INSERT INTO Activity (UserID, Title, Description, Type, Date, StartTime, EndTime, IsRecurring, 
+                    RecurringActivityID) VALUES ({user_id}, '{title}', '{description}', {type}, '{date}', '{start_time}'
+                    , '{end_time}', {is_recurring}, {recurring_activity_id});"""
+
+        try:
+            self.db.insert(insert_sql)
+        except Exception as e:
+            print(f"The error '{e}' occurred")
+            return False
+
+        return True
+
+    def insert_recurring_activity(self, recurring_pattern, recurring_end_date):
+        """ insert a recurring activity into the database """
+
+        insert_sql = f"""INSERT INTO RecurringActivity (RecurrencePattern, RecurrenceEndDate)
+                        VALUES ('{recurring_pattern}', '{recurring_end_date}');"""
+
+        recurring_activity_id = self.db.insert_with_output_id(insert_sql)
+
+        return recurring_activity_id
