0e098081900e1a2ebe29b511b7d18135458a6d7b
Sadra <sadra3tarehdan@gmail.com>
2024-05-08 18:46:31 -0400

    removed funny change

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 DEBUG = True
 START_OF_DAY_HOUR = 13
 END_OF_DAY_HOUR = 17
-TIME_INTERVAL_TYPE = 3  # 1- 15 min, 2- 30 min, 3- 1 hour (Hello)
+TIME_INTERVAL_TYPE = 3  # 1- 15 min, 2- 30 min, 3- 1 hour
